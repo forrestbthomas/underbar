7743592196624cd870c7152ae68fda8020331aa4
forrestbthomas <forrestbthomas@gmail.com>
2014-05-01 11:06:51 -0700

    passed _.some

--- src/underbar.js
+++ src/underbar.js
@@ -213,6 +213,7 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
     return !_.every(collection, function(val) {
       return !iterator(val);
     });
