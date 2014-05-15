04dfc965bca37bf1df54772954f14e9506188066
forrestbthomas <forrestbthomas@gmail.com>
2014-05-15 13:54:24 -0700

    passed intersection

--- src/underbar.js
+++ src/underbar.js
@@ -443,6 +443,11 @@ var _ = {};
   // Take the difference between one array and a number of other arrays.
   // Only the elements present in just the first array will remain.
   _.difference = function(array) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    _.each(array, function(val){
+      
+    })
+    
   };
 
 
